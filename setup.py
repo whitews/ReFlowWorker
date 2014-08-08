907ac46cc88603079cf8354e2e405b5366067070
Scott White <whitews@gmail.com>
2014-08-08 19:47:08 -0400

    bump version to 0.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ReFlowWorker',
-    version='0.0.5',
+    version='0.0.6',
     author='Scott White',
     author_email='scott.white@duke.edu',
     packages=['reflowworker'],
@@ -12,7 +12,7 @@ setup(
     install_requires=[
         'requests>=1.1.0',
         'numpy>=1.6',
-        'reflowrestclient>=0.0.5',
+        'reflowrestclient>=0.0.9',
         'flowio',
         'flowutils',
         'flowstats'
