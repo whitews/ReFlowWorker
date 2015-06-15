e6d6dcd2c2381ed35e24bfd8e68ed77042c022ef
Scott White <whitews@gmail.com>
2015-06-15 12:38:35 -0400

    create custom ProcessingError for reporting back to ReFlow server

---
+++ reflowworker/processing_error.py
@@ -0,0 +1,9 @@
+class ProcessingError(Exception):
+    """
+    Provides more user-friendly error message to the ReFlow server. In many
+    cases ProcessingError acts as a translation for the actual error, and
+    the original error/exception is logged and a more useful message is
+    given.
+    """
+    def __init__(self, message):
+        super(ProcessingError, self).__init__(message)
