5b5119cdcb1a3a9cc8561314390d83591d21bb65
Scott White <whitews@gmail.com>
2016-02-11 11:38:45 -0500

    bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ReFlowWorker',
-    version='0.1.0b',
+    version='0.2.0',
     author='Scott White',
     author_email='scott.white@duke.edu',
     packages=['reflowworker'],
@@ -10,12 +10,12 @@ setup(
     license='LICENSE.txt',
     description='Client side worker for a ReFlow server for distributed processing',
     install_requires=[
-        'requests>=1.1.0',
-        'numpy>=1.6',
-        'pycuda>=2013.1.1',
-        'reflowrestclient',
+        'requests (>=1.1.0)',
+        'numpy (>=1.6)',
+        'pycuda (>=2013.1.1)',
+        'reflowrestclient (==0.2.0)',
         'flowio',
         'flowutils==0.3',
-        'flowstats'
+        'flowstats==0.4'
     ]
 )
